c8319fca2a486a09896188ca5a1782f6b6de66ad
mjamer <maciej.jamer@af.agency>
2023-01-13 13:47:08 +0100

    endpoint delete user by id

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,27 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.delete('/:userId', async (req, res) => {
+    try {
+      const removedOUser = await User.findOneAndDelete({
+        _id: req.params.userId,
+      });
+      console.log(req.params.userId)
+      res.status(200).json({
+        _id: removedOUser._id,
+        name: removedOUser.name,
+        email: removedOUser.email,
+        token: removedOUser.token,
+        emailVerified: removedOUser.emailVerified,
+        created_at: removedOUser.created_at,
+        last_login_at: removedOUser.last_login_at,
+        roles: removedOUser.roles,
+        phoneNumber: removedOUser.phoneNumber,
+        photo: removedOUser.photo
+    });
+    } catch (err) {
+      res.json({ message: err });
+    }
+});
+
 module.exports = router;
